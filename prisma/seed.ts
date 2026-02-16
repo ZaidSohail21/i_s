ab93c50120d50b83a585a511622c350a85675f30
Muhammad Zaid Sohail <zaidsohail4321@gmail.com>
2026-02-17 00:39:31 +0500

    Dashboard Completed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,4 @@
-// import { PrismaClient } from "@prisma/client";
-// const prisma = new PrismaClient();
 
-
-// //Creating a sample products for demo user
-// async function main() {
-//     const DemoUserId="clh5lnu80000v0b60f4f4k3r3"; // Replace with your actual user ID
-//     data : Array.from({length:25}).map((_,i)=>({  
-//         userId: DemoUserId,
-//         name: `Product ${i+1}`,
-//         sku: `SKU-${i+1}`,
-//         price: 10.99 + i,
-//         quantity: 10 + i,
-//         lowStockAt: 5 + i,
-//         createdAt: new Date(Date.now() - i * 86400000), // Spread createdAt over past days
-//     }));
-//     console.log("Seeding products...");
-//     console.log(`Inserting 25 products for demo user Id : ${DemoUserId}`);
-//     console.log("DATABASE_URL:", process.env.DATABASE_URL);
-// }
-
-// main()
-// .catch((e) => {
-//     console.error(e);
-//     process.exit(1);
-// })
-// .finally(async () => {
-//     await prisma.$disconnect();
-// });
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
